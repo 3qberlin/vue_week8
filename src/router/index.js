2d4726334be58f1a07a62f92451a8a31b8d9a535
Berlin <3q.berlin@gmail.com>
2024-03-02 14:54:48 +0800

    feat: 📟 定義 product 單一產品頁 作 動態路由
    ✅ Closes: 807ac76

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
         component: () => import('../views/FrontEnd/ProductsView.vue'),
       },
       {
-        path: 'product',
+        path: 'product/:id',
         name: 'product',
         component: () => import('../views/FrontEnd/ProductView.vue'),
       },
