5452475b15b3c34f0a7b2c932ef8b609a05b0a7e
Berlin <3q.berlin@gmail.com>
2024-03-19 17:25:26 +0800

    feat: 📟 footer in component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,71 @@
+<template>
+  <footer>
+    <div class="bg-dark py-5">
+      <div class="container">
+        <div
+          class="d-flex align-items-center justify-content-between
+           text-white mb-md-7 mb-4"
+        >
+          <img
+            src="https://hackmd.io/_uploads/S1atYtYap.png"
+            alt="logo"
+            style="width: 140px; object-fit: cover; cursor: pointer"
+          />
+          <ul class="d-flex list-unstyled mb-0 h4">
+            <li>
+              <a href="#" class="text-white mx-3"
+                ><i class="bi bi-facebook"></i
+              ></a>
+            </li>
+            <li>
+              <a href="#" class="text-white mx-3"
+                ><i class="bi bi-instagram"></i
+              ></a>
+            </li>
+            <li>
+              <a href="#" class="text-white ms-3"><i class="bi bi-line"></i></a>
+            </li>
+            <li class="ms-3 d-flex flex-wrap">
+              <a
+                href="https://3qberlin.github.io/vue_week7/#/"
+                class="text-white ms-3 d-flex flex-wrap"
+                ><i class="bi bi-door-open"></i
+              ></a>
+              <a
+                class="fs-6 d-block-block w-100 text-white text-decoration-none"
+                href="https://3qberlin.github.io/vue_week7/#/"
+                >管理者</a
+              >
+            </li>
+          </ul>
+        </div>
+        <div
+          class="d-flex flex-column flex-md-row justify-content-between
+           align-items-md-end align-items-start text-white"
+        >
+          <div class="mb-md-0 mb-1">
+            <p class="mb-0">
+              <a
+                href=":tel:02-3456-7890"
+                class="text-white text-decoration-none"
+                >02-3456-7890</a
+              >
+            </p>
+            <p class="mb-0">
+              <a
+                href="mailto:serviceTravel@gmail.com"
+                class="text-white text-decoration-none"
+                >serviceTravel@gmail.com</a
+              >
+            </p>
+          </div>
+          <p class="mb-0">© 2024 Home Travel All Rights Reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
