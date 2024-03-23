5482d09f7ffed46e286b50ad552b30c6a6b4e290
Berlin <3q.berlin@gmail.com>
2024-03-23 20:10:41 +0800

    fix: 🎯 文字位置

--- src/views/FrontEnd/CheckoutView.vue
+++ src/views/FrontEnd/CheckoutView.vue
@@ -24,8 +24,8 @@
               <p class="mb-0 fw-bold">{{ item.product.num }} 組</p>
             </div>
           </div>
+          <h4 class="fw-bold mb-4">訂單資料</h4>
           <table class="table mt-4 border-top border-bottom text-muted">
-            <h4 class="fw-bold mb-4">訂單資料</h4>
             <tbody>
               <tr>
                 <th scope="row" class="border-0 px-0 pt-4 font-weight-normal">
